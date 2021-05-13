47f6ed5c1b1fa1515e97e022f525fc38e8892ed8
lingowu <lingowu@tencent.com>
2021-05-13 22:36:55 +0800

    原型模式的完善

---
+++ CreationalPatten/Prototype/Protype.h
@@ -0,0 +1,40 @@
+//
+//  Protype.h
+//  Protype
+//
+//  Created by lingowu on 2021/5/12.
+//
+
+#ifndef Protype_h
+#define Protype_h
+
+#include <iostream>
+
+using namespace std;
+
+// 猴子
+class Monkey {
+public:
+    virtual ~Monkey(){}
+    virtual Monkey* Clone() = 0;  // 克隆
+    virtual void Play() = 0;  // 玩耍
+};
+
+// 孙悟空
+class WuKong : public Monkey {
+public:
+    WuKong(string name):name(name) {}
+    ~WuKong() {}
+
+    // 拷贝构造函数
+    WuKong(const WuKong &other) { name = other.name;}
+    // 调用拷贝构造函数
+    Monkey* Clone() { return new WuKong(*this);}
+    void Play() { cout << name << "玩金箍棒" << endl;}
+
+private:
+    string name;
+};
+
+
+#endif /* Protype_h */
--- CreationalPatten/Prototype/main.cpp
+++ CreationalPatten/Prototype/main.cpp
@@ -1,14 +1,28 @@
 //
 //  main.cpp
-//  Prototype
+//  Protype
 //
-//  Created by lingowu on 2021/5/13.
+//  Created by lingowu on 2021/5/12.
 //
 
 #include <iostream>
+#include "Protype.h"
+
+#define SAFE_DELETE(p) { if(p){delete(p); (p)=nullptr;} }
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    // 孙悟空
+    Monkey *swk = new WuKong("齐天大圣");
+    // 克隆猴子猴孙
+    Monkey *swk1 = swk->Clone();
+    Monkey *swk2 = swk1->Clone();
+    
+    swk1->Play();
+    swk2->Play();
+    
+    SAFE_DELETE(swk);
+    SAFE_DELETE(swk2);
+    SAFE_DELETE(swk1);
+    
     return 0;
 }
